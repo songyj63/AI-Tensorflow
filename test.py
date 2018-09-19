796aee1b9f03524779e45c80a0ed0aff12ab09b6
YJ Song <songdevackor@gmail.com>
2018-09-19 18:50:25 +0900

    chapter 7 - CNN

--- test.py
+++ test.py
@@ -1,35 +1,61 @@
+# 이미지 처리 분야에서 가장 유명한 신경망 모델인 CNN 을 이용하여 더 높은 인식률을 만들어봅니다.
 import tensorflow as tf
+
 from tensorflow.examples.tutorials.mnist import input_data
-import matplotlib.pyplot as plt
-import numpy as np
+mnist = input_data.read_data_sets("./mnist/data/", one_hot=True)
 
-mnist = input_data.read_data_sets("./06 - MNIST/mnist/data/", one_hot=True)
-
-X = tf.placeholder(tf.float32, [None, 784])
+#########
+# 신경망 모델 구성
+######
+# 기존 모델에서는 입력 값을 28x28 하나의 차원으로 구성하였으나,
+# CNN 모델을 사용하기 위해 2차원 평면과 특성치의 형태를 갖는 구조로 만듭니다.
+X = tf.placeholder(tf.float32, [None, 28, 28, 1])
 Y = tf.placeholder(tf.float32, [None, 10])
-# keep_prob = tf.placeholder(tf.float32)
-is_training = tf.placeholder(tf.bool)
+keep_prob = tf.placeholder(tf.float32)
 
-W1 = tf.Variable(tf.random_normal([784, 256], stddev=0.01))
-b1 = tf.Variable(tf.random_normal([1, 256], stddev=0.01))
-L1 = tf.nn.relu(tf.matmul(X, W1) + b1)
-L1 = tf.layers.batch_normalization(L1, training=is_training)
+# 각각의 변수와 레이어는 다음과 같은 형태로 구성됩니다.
+# W1 [3 3 1 32] -> [3 3]: 커널 크기, 1: 입력값 X 의 특성수, 32: 필터 갯수
+# L1 Conv shape=(?, 28, 28, 32)
+#    Pool     ->(?, 14, 14, 32)
+W1 = tf.Variable(tf.random_normal([3, 3, 1, 32], stddev=0.01))
+# tf.nn.conv2d 를 이용해 한칸씩 움직이는 컨볼루션 레이어를 쉽게 만들 수 있습니다.
+# padding='SAME' 은 커널 슬라이딩시 최외곽에서 한칸 밖으로 더 움직이는 옵션
+L1 = tf.nn.conv2d(X, W1, strides=[1, 1, 1, 1], padding='SAME')
+L1 = tf.nn.relu(L1)
+# Pooling 역시 tf.nn.max_pool 을 이용하여 쉽게 구성할 수 있습니다.
+L1 = tf.nn.max_pool(L1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+# L1 = tf.nn.dropout(L1, keep_prob)
 
-W2 = tf.Variable(tf.random_normal([256, 256], stddev=0.01))
-b2 = tf.Variable(tf.random_normal([1, 256], stddev=0.01))
-L2 = tf.nn.relu(tf.matmul(L1, W2) + b2)
-L2 = tf.layers.batch_normalization(L2, training=is_training)
+# L2 Conv shape=(?, 14, 14, 64)
+#    Pool     ->(?, 7, 7, 64)
+# W2 의 [3, 3, 32, 64] 에서 32 는 L1 에서 출력된 W1 의 마지막 차원, 필터의 크기 입니다.
+W2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01))
+L2 = tf.nn.conv2d(L1, W2, strides=[1, 1, 1, 1], padding='SAME')
+L2 = tf.nn.relu(L2)
+L2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+# L2 = tf.nn.dropout(L2, keep_prob)
 
-W3 = tf.Variable(tf.random_normal([256, 10], stddev=0.01))
-b3 = tf.Variable(tf.random_normal([1, 10], stddev=0.01))
-model = tf.matmul(L2, W3) + b3
+# FC 레이어: 입력값 7x7x64 -> 출력값 256
+# Full connect를 위해 직전의 Pool 사이즈인 (?, 7, 7, 64) 를 참고하여 차원을 줄여줍니다.
+#    Reshape  ->(?, 256)
+W3 = tf.Variable(tf.random_normal([7 * 7 * 64, 256], stddev=0.01))
+L3 = tf.reshape(L2, [-1, 7 * 7 * 64])
+L3 = tf.matmul(L3, W3)
+L3 = tf.nn.relu(L3)
+L3 = tf.nn.dropout(L3, keep_prob)
 
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=Y, logits=model))
-update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-with tf.control_dependencies(update_ops):
-    optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
+# 최종 출력값 L3 에서의 출력 256개를 입력값으로 받아서 0~9 레이블인 10개의 출력값을 만듭니다.
+W4 = tf.Variable(tf.random_normal([256, 10], stddev=0.01))
+model = tf.matmul(L3, W4)
 
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=model, labels=Y))
+optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
+# 최적화 함수를 RMSPropOptimizer 로 바꿔서 결과를 확인해봅시다.
+# optimizer = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
 
+#########
+# 신경망 모델 학습
+######
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
@@ -42,27 +68,27 @@ for epoch in range(15):
 
     for i in range(total_batch):
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-        _, cost_val = sess.run([optimizer, cost], feed_dict={X: batch_xs, Y: batch_ys, is_training: True})
+        # 이미지 데이터를 CNN 모델을 위한 자료형태인 [28 28 1] 의 형태로 재구성합니다.
+        batch_xs = batch_xs.reshape(-1, 28, 28, 1)
 
+        _, cost_val = sess.run([optimizer, cost],
+                               feed_dict={X: batch_xs,
+                                          Y: batch_ys,
+                                          keep_prob: 0.7})
         total_cost += cost_val
+        print(cost_val)
 
-    print('Epoch: %04d' % (epoch + 1), 'Avg. cost = {:.3f}'.format(total_cost / total_batch))
+    print('Epoch:', '%04d' % (epoch + 1),
+          'Avg. cost =', '{:.3f}'.format(total_cost / total_batch))
 
+print('최적화 완료!')
 
+#########
+# 결과 확인
+######
 is_correct = tf.equal(tf.argmax(model, 1), tf.argmax(Y, 1))
-accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))*100
-
-print('정확도: %.2f %%' %sess.run(accuracy, feed_dict={X: mnist.test.images, Y: mnist.test.labels, is_training: False}))
-
-labels = sess.run(model, feed_dict={X: mnist.test.images,
-                                    Y: mnist.test.labels,
-                                    is_training: False})
-
-fig = plt.figure()
-
-for i in range(10):
-    subplot = fig.add_subplot(2, 5, i+1)
-    subplot.set_title('%d' % np.argmax(labels[i]))
-    subplot.imshow(mnist.test.images[i].reshape((28, 28)), cmap=plt.cm.gray_r)
-
-plt.show()
+accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
+print('정확도:', sess.run(accuracy,
+                        feed_dict={X: mnist.test.images.reshape(-1, 28, 28, 1),
+                                   Y: mnist.test.labels,
+                                   keep_prob: 1}))
