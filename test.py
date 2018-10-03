28092573faa1ce0fe69a4201fe0b64a924ebd21b
YJ <songyj63@gmail.com>
2018-10-03 12:38:03 +0900

    final

--- test.py
+++ test.py
@@ -1,100 +1,137 @@
-# 이미지 처리 분야에서 가장 유명한 신경망 모델인 CNN 을 이용하여 더 높은 인식률을 만들어봅니다.
 import tensorflow as tf
+import numpy as np
 
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("./mnist/data/", one_hot=True)
+char_arr = [c for c in 'SEPabcdefghijklmnopqrstuvwxyz단어나무놀이소녀키스사랑']
+
+num_dic = {n: i for i, n in enumerate(char_arr)}
+dic_len = len(num_dic)
+
+# 영어를 한글로 번역하기 위한 학습 데이터
+seq_data = [['word', '단어'], ['wood', '나무'],
+            ['game', '놀이'], ['girl', '소녀'],
+            ['kiss', '키스'], ['love', '사랑']]
+
+
+def make_batch(seq_data):
+    input_batch = []
+    output_batch = []
+    target_batch = []
+
+    for seq in seq_data:
+        # 인코더 셀의 입력값. 입력단어의 글자들을 한글자씩 떼어 배열로 만든다.
+        input = [num_dic[n] for n in seq[0]]
+        # 디코더 셀의 입력값. 시작을 나타내는 S 심볼을 맨 앞에 붙여준다.
+        output = [num_dic[n] for n in ('S' + seq[1])]
+        # 학습을 위해 비교할 디코더 셀의 출력값. 끝나는 것을 알려주기 위해 마지막에 E 를 붙인다.
+        target = [num_dic[n] for n in (seq[1] + 'E')]
+
+        input_batch.append(np.eye(dic_len)[input])
+        output_batch.append(np.eye(dic_len)[output])
+        # 출력값만 one-hot 인코딩이 아님 (sparse_softmax_cross_entropy_with_logits 사용)
+        target_batch.append(target)
+
+    return input_batch, output_batch, target_batch
+
+
+learning_rate = 0.01
+n_hidden = 128
+total_epoch = 100
+n_class = n_input = dic_len
+
 
 #########
 # 신경망 모델 구성
 ######
-# 기존 모델에서는 입력 값을 28x28 하나의 차원으로 구성하였으나,
-# CNN 모델을 사용하기 위해 2차원 평면과 특성치의 형태를 갖는 구조로 만듭니다.
-X = tf.placeholder(tf.float32, [None, 28, 28, 1])
-Y = tf.placeholder(tf.float32, [None, 10])
-keep_prob = tf.placeholder(tf.float32)
+# Seq2Seq 모델은 인코더의 입력과 디코더의 입력의 형식이 같다.
+# [batch size, time steps, input size]
+enc_input = tf.placeholder(tf.float32, [None, None, n_input])
+dec_input = tf.placeholder(tf.float32, [None, None, n_input])
+# [batch size, time steps]
+targets = tf.placeholder(tf.int64, [None, None])
 
 
-L1 = tf.layers.conv2d(X, 32, [3, 3], activation=tf.nn.relu, padding='SAME')
-L1 = tf.layers.max_pooling2d(L1, [2, 2], [2, 2], padding='SAME')
+# 인코더 셀을 구성한다.
+with tf.variable_scope('encode'):
+    enc_cell = tf.nn.rnn_cell.BasicRNNCell(n_hidden)
+    enc_cell = tf.nn.rnn_cell.DropoutWrapper(enc_cell, output_keep_prob=0.5)
 
-# L2 Conv shape=(?, 14, 14, 64)
-#    Pool     ->(?, 7, 7, 64)
-# W2 의 [3, 3, 32, 64] 에서 32 는 L1 에서 출력된 W1 의 마지막 차원, 필터의 크기 입니다.
-W2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01))
-L2 = tf.nn.conv2d(L1, W2, strides=[1, 1, 1, 1], padding='SAME')
-L2 = tf.nn.relu(L2)
-L2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-# L2 = tf.nn.dropout(L2, keep_prob)
+    outputs, enc_states = tf.nn.dynamic_rnn(enc_cell, enc_input,
+                                            dtype=tf.float32)
 
-L2 = tf.layers.conv2d(L1, 64)
+# 디코더 셀을 구성한다.
+with tf.variable_scope('decode'):
+    dec_cell = tf.nn.rnn_cell.BasicRNNCell(n_hidden)
+    dec_cell = tf.nn.rnn_cell.DropoutWrapper(dec_cell, output_keep_prob=0.5)
 
-# FC 레이어: 입력값 7x7x64 -> 출력값 256
-# Full connect를 위해 직전의 Pool 사이즈인 (?, 7, 7, 64) 를 참고하여 차원을 줄여줍니다.
-#    Reshape  ->(?, 256)
-W3 = tf.Variable(tf.random_normal([7 * 7 * 64, 256], stddev=0.01))
-L3 = tf.reshape(L2, [-1, 7 * 7 * 64])
-L3 = tf.matmul(L3, W3)
-L3 = tf.nn.relu(L3)
-L3 = tf.nn.dropout(L3, keep_prob)
+    # Seq2Seq 모델은 인코더 셀의 최종 상태값을
+    # 디코더 셀의 초기 상태값으로 넣어주는 것이 핵심.
+    outputs, dec_states = tf.nn.dynamic_rnn(dec_cell, dec_input,
+                                            initial_state=enc_states,
+                                            dtype=tf.float32)
 
-# 최종 출력값 L3 에서의 출력 256개를 입력값으로 받아서 0~9 레이블인 10개의 출력값을 만듭니다.
-W4 = tf.Variable(tf.random_normal([256, 10], stddev=0.01))
-model = tf.matmul(L3, W4)
 
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=model, labels=Y))
-optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
-# 최적화 함수를 RMSPropOptimizer 로 바꿔서 결과를 확인해봅시다.
-# optimizer = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
+model = tf.layers.dense(outputs, n_class, activation=None)
+cost = tf.reduce_mean(
+            tf.nn.sparse_softmax_cross_entropy_with_logits(
+                logits=model, labels=targets))
+
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
 
 #########
 # 신경망 모델 학습
 ######
-init = tf.global_variables_initializer()
 sess = tf.Session()
-sess.run(init)
+sess.run(tf.global_variables_initializer())
 
-batch_size = 100
-total_batch = int(mnist.train.num_examples / batch_size)
+input_batch, output_batch, target_batch = make_batch(seq_data)
 
-for epoch in range(15):
-    total_cost = 0
-
-    for i in range(total_batch):
-        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-
-        # 이미지 데이터를 CNN 모델을 위한 자료형태인 [28 28 1] 의 형태로 재구성합니다.
-        batch_xs = batch_xs.reshape(-1, 28, 28, 1)
-
-        _, cost_val = sess.run([optimizer, cost],
-                               feed_dict={X: batch_xs,
-                                          Y: batch_ys,
-                                          keep_prob: 0.7})
-        total_cost += cost_val
+for epoch in range(total_epoch):
+    _, loss = sess.run([optimizer, cost],
+                       feed_dict={enc_input: input_batch,
+                                  dec_input: output_batch,
+                                  targets: target_batch})
 
     print('Epoch:', '%04d' % (epoch + 1),
-          'Avg. cost =', '{:.3f}'.format(total_cost / total_batch))
+          'cost =', '{:.6f}'.format(loss))
 
 print('최적화 완료!')
 
 #########
-# 결과 확인
+# 번역 테스트
 ######
-is_correct = tf.equal(tf.argmax(model, 1), tf.argmax(Y, 1))
-accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
+# 단어를 입력받아 번역 단어를 예측하고 디코딩하는 함수
+def translate(word):
+    # 이 모델은 입력값과 출력값 데이터로 [영어단어, 한글단어] 사용하지만,
+    # 예측시에는 한글단어를 알지 못하므로, 디코더의 입출력값을 의미 없는 값인 P 값으로 채운다.
+    # ['word', 'PPPP']
+    seq_data = [word, 'P' * len(word)]
 
-total_acc = 0
-total_batch_test = int(mnist.test.num_examples / batch_size)
-for i in range(total_batch_test):
-    batch_x_test, batch_y_test = mnist.test.next_batch(batch_size)
-    batch_x_test = batch_x_test.reshape(-1, 28, 28, 1)
+    input_batch, output_batch, target_batch = make_batch([seq_data])
 
-    total_acc += sess.run(accuracy, feed_dict={X: batch_x_test,
-                                   Y: batch_y_test,
-                                   keep_prob: 1})
-    print('정확도:', sess.run(accuracy,
-                        feed_dict={X: batch_x_test,
-                                   Y: batch_y_test,
-                                   keep_prob: 1}), 'total_acc: %.2f' %total_acc)
+    # 결과가 [batch size, time step, input] 으로 나오기 때문에,
+    # 2번째 차원인 input 차원을 argmax 로 취해 가장 확률이 높은 글자를 예측 값으로 만든다.
+    prediction = tf.argmax(model, 2)
 
-total_acc = total_acc / total_batch_test * 100
-print('최종 정확도: %.2f' %total_acc)
+    result = sess.run(prediction,
+                      feed_dict={enc_input: input_batch,
+                                 dec_input: output_batch,
+                                 targets: target_batch})
+
+    # 결과 값인 숫자의 인덱스에 해당하는 글자를 가져와 글자 배열을 만든다.
+    decoded = [char_arr[i] for i in result[0]]
+
+    # 출력의 끝을 의미하는 'E' 이후의 글자들을 제거하고 문자열로 만든다.
+    end = decoded.index('E')
+    translated = ''.join(decoded[:end])
+
+    return translated
+
+
+print('\n=== 번역 테스트 ===')
+
+print('word ->', translate('word'))
+print('wodr ->', translate('wodr'))
+print('love ->', translate('love'))
+print('loev ->', translate('loev'))
+print('abcd ->', translate('abcd'))
+print('kids ->', translate('kids'))
